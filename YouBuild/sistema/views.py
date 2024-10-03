48d3b599fa46530e0d6c3b44677a4b1395421cb1
Valeria Daza <valeriadaza54@gmail.com>
2024-10-03 01:28:38 -0400

    Revert "Feature/lista productos"

--- YouBuild/sistema/views.py
+++ YouBuild/sistema/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import ProductoDb, CategoriaDb
+from django.shortcuts import render,get_object_or_404
+from .models import ProductoDb,CategoriaDb
+#from django.http import HttpResponse
+# Create your views here.
 
-# Vista principal
 def IndexView(request):
-    productos = ProductoDb.objects.all().order_by("id")  # Asignar productos a la variable
-    return render(request, "index.html", {"productos": productos})  # Usar "productos" en el contexto
+    
+    objeto = ProductoDb.objects.all().order_by("id")
+    
+    return render(request,"index.html",{"objeto":objeto})
 
-def ProductoView(request, id):
-    producto = get_object_or_404(ProductoDb, id=id)
-    return render(request, "productos.html", {"producto": producto})
 
+def ProductoView(request,id):
+    producto = get_object_or_404(ProductoDb,id=id)
+    return render(request,"productos.html",{"producto":producto})
