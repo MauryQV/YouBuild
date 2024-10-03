d11bdd1192c948c1e111eaa85e9ccde8ed813039
Valeria <valeriadaza54@gmail.com>
2024-10-03 03:33:58 -0400

    Implementacion de Productos Disponibles

--- YouBuild/sistema/views.py
+++ YouBuild/sistema/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render,get_object_or_404
-from .models import ProductoDb,CategoriaDb
-#from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import ProductoDb, CategoriaDb
 
+# Vista principal
 def IndexView(request):
-    
-    objeto = ProductoDb.objects.all().order_by("id")
-    
-    return render(request,"index.html",{"objeto":objeto})
+    productos = ProductoDb.objects.all().order_by("id")  
+    return render(request, "index.html", {"productos": productos}) 
 
-
-def ProductoView(request,id):
-    producto = get_object_or_404(ProductoDb,id=id)
-    return render(request,"productos.html",{"producto":producto})
+def ProductoView(request, id):
+    producto = get_object_or_404(ProductoDb, id=id)
+    return render(request, "productos.html", {"producto": producto})
