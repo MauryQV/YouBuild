9894be390206514dfe913f0eeef5aa97e12e1ea6
MauryQV <qmaury30@gmail.com>
2024-10-03 08:00:23 -0400

    backend carrusel

--- YouBuild/sistema/views.py
+++ YouBuild/sistema/views.py
@@ -4,9 +4,11 @@ from .models import ProductoDb, CategoriaDb, CarruselDB
 # Vista principal
 
 
-def IndexView(request):
+
+def IndexView(request): 
     productos = ProductoDb.objects.all().order_by("id")  
-    return render(request, "index.html", {"producto": productos}) 
+    carruseles = CarruselDB.objects.all().order_by("id")
+    return render(request, "index.html", {"producto": productos, "carrusel": carruseles})
 
 def ProductoView(request, id):
     producto = get_object_or_404(ProductoDb, id=id)
