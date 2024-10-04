ef77db4e9eeb789fb4ca816502fae3d0ec311288
JAFETLP <jafetcana@gmail.com>
2024-10-04 00:05:41 -0300

    new cambios1

--- YouBuild/sistema/urls.py
+++ YouBuild/sistema/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from sistema.views import IndexView, ProductoView
+from sistema.views import IndexView, ProductoView, BuscarView
 
 urlpatterns = [
     path('', IndexView, name='index'),
     path('producto/<int:id>/', ProductoView, name='detalle_producto'),
+    path('buscar/', BuscarView, name='buscar'),
 ]
 
 if settings.DEBUG:
