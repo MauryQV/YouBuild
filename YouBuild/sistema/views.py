b8d8107fc295c6683dbb7bcfe78c2ff52409e0ca
JAFETLP <jafetcana@gmail.com>
2024-10-08 16:31:34 -0300

    reparacion de num visitas

--- YouBuild/sistema/views.py
+++ YouBuild/sistema/views.py
@@ -4,13 +4,14 @@ from .models import ProductoDb, CategoriaDb, CarruselDB
 # Vista principal
 
 def IndexView(request): 
-    productos = ProductoDb.objects.all().order_by('-visitas')  # Ordena por visitas de mayor a menor
+    productos = ProductoDb.objects.all().order_by("id")  
     carruseles = CarruselDB.objects.all().order_by("id")
     return render(request, "index.html", {"producto": productos, "carrusel": carruseles})
 
 def ProductoView(request, id):
     producto = get_object_or_404(ProductoDb, id=id)
-    return render(request, "detalle_producto.html", {"producto": producto})
+    imagenes = producto.imagenes.all()
+    return render(request, "detalle_producto.html", {"producto": producto,})
 
 def BuscarView(request):
     q = request.GET.get('q', '')
