68596963ae64fa64de2179171056ebbda4e0afb4
Valeria <valeriadaza54@gmail.com>
2024-10-08 17:26:37 -0400

    Vista de Carrito de Compras

--- YouBuild/sistema/urls.py
+++ YouBuild/sistema/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from sistema.views import IndexView, ProductoView, BuscarView, CheckoutView
+from sistema.views import IndexView, ProductoView, BuscarView, CheckoutView,carrito_view,eliminar_producto
 
 urlpatterns = [
     path('', IndexView, name='index'),
@@ -10,6 +10,8 @@ urlpatterns = [
     path('buscar/', BuscarView, name='buscar'),
     
     path('layout/',CheckoutView,name='layout'),
+    path('carrito/', carrito_view, name='Carrito'),
+    path('carrito/eliminar/<int:item_id>/', eliminar_producto, name='eliminar_producto'),
 ]
 
 if settings.DEBUG:
