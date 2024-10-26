307e9e0d7a7827ada6b4d3d6c43451b34dfeab15
Jhoel Gustavo Aiza Arce <8therex8@gmail.com>
2024-10-26 00:43:06 -0400

    Agregado mis aportes sobre la nueva interfaz del formulario de Crear Cuenta en la rama Gustavo-2doSprint

--- YouBuild/sistema/urls.py
+++ YouBuild/sistema/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
     path('ajax/cargar-provincias/', cargar_provincias, name='ajax_cargar_provincias'),
     path('ajax/cargar-municipios/', cargar_municipios, name='ajax_cargar_municipios'),
-    path('api/registro/', RegistroUsuario.as_view(), name='registro_usuario'),
+    # Agrega la línea siguiente para crear la cuenta
+    path('crear-cuenta/', CrearCuentaView, name='crear_cuenta'),  # <-- Aquí
+   # path('api/registro/', RegistroUsuario.as_view(), name='registro_usuario'),
 
     
     path('test/',test,name="testeo")
