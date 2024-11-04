ad7e2bd46066a07bc45472f91025674da3b6b1c8
VICENTE MAURICIO QUINSAMOLLE VARBAS <qmaury30@gmail.com>
2024-11-04 17:23:44 -0400

    rama fernanda cart-count

--- YouBuild/sistema/context_processors.py
+++ YouBuild/sistema/context_processors.py
@@ -1,6 +1,17 @@
-from sistema.models import UsuarioDB
+from sistema.models import UsuarioDB, CarritoProductoDB
+from django.db.models import Sum
 
 def cart_count(request):
-    cart_count = request.session.get('cart_count', 0)
+    if request.user.is_authenticated:  # Only fetch the cart count for logged-in users
+        try:
+            # Retrieve the logged-in user's cart count from the database
+            usuario = request.user.usuariodb
+            cart_count = CarritoProductoDB.objects.filter(carrito_fk__usuario_fk=usuario).aggregate(
+                total_quantity=Sum('cantidad')
+            )['total_quantity'] or 0
+        except UsuarioDB.DoesNotExist:
+            # If the user does not have a UsuarioDB profile, default to 0
+            cart_count = 0
+    else:
+        cart_count = 0
     return {'cart_count': cart_count}
-
