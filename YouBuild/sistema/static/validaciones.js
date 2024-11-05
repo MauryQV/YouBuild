a62b67f337dd5ea6dbb07f4deb841a90af7df415
Jhazmin <portuguez.colque.jhazmin25@gmail.com>
2024-11-05 09:28:30 -0400

    Agregación de validaciones en el campo Buscar(layout.html) y creación de validaciones.js

--- YouBuild/sistema/static/validaciones.js
+++ YouBuild/sistema/static/validaciones.js
@@ -1,9 +1,9 @@
 // static/js/validaciones.js
 function validateSearch() {
-    const searchField = document.querySelector('input[name="q"]');
-    if (searchField.value.trim() === "") {
-      alert("Por favor, ingrese un término de búsqueda.");
-      return false;
-    }
-    return true;
-  }  
+  const searchField = document.querySelector('input[name="q"]');
+  if (searchField.value.trim() === "") {
+    alert("Por favor, ingrese un término de búsqueda.");
+    return false;
+  }
+  return true;
+}
