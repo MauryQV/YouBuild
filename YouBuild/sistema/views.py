a0b588e7f9abea0b3c796e13ff05f961a1405913
Valeria <valeriadaza54@gmail.com>
2024-11-21 10:24:51 -0400

    Modificaciones en top bar 2

--- YouBuild/sistema/views.py
+++ YouBuild/sistema/views.py
@@ -640,7 +640,7 @@ def procesar_transaccion(request):
 
     return JsonResponse({'error': 'Método no permitido'}, status=405)
 
-import qrcode
+import qrcode 
 from io import BytesIO
 from django.core.files.base import ContentFile
 
